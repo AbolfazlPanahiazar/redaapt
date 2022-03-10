76d0d96d52428d2a20112ea1bd65fcd0b8d799d7
Abolfazl Panahiazar <abolfazlpanahiazar@gmail.com>
2022-03-10 07:41:10 -0500

    Add components to App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,20 @@
 import { FC } from "react";
 
+import { Navbar, Welcome, Footer, Services, Transactions } from "./components";
 import "./App.css";
 
 const App: FC = () => {
-  return <div></div>;
+  return (
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
