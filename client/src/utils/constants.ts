ee5e444bea82385b3db5ebb00184252a183cce53
Abolfazl Panahiazar <abolfazlpanahiazar@gmail.com>
2022-03-12 12:51:37 -0500

    Add contractAddress and abi on ribkeby to client project

---
+++ client/src/utils/constants.ts
@@ -0,0 +1,4 @@
+import abi from './transactions.json';
+
+export const contractAddress = '0x4Ec76bFaCceCC8c7d28D67d66F4dF465380C7b20';
+export const contractABI = abi.abi;
--- smart-contracts/hardhat.config.js
+++ smart-contracts/hardhat.config.js
@@ -1,12 +1,10 @@
-// https://eth-ropsten.alchemyapi.io/v2/9_joBZyX8vb1Yn8h1jkVOB3rU8IJoDeM
-
 require("@nomiclabs/hardhat-waffle");
 
 module.exports = {
   solidity: "0.8.0",
   networks: {
-    ropsten: {
-      url: "https://eth-ropsten.alchemyapi.io/v2/9_joBZyX8vb1Yn8h1jkVOB3rU8IJoDeM",
+    rinkeby: {
+      url: "https://eth-rinkeby.alchemyapi.io/v2/HskXgPhCcajHNXC3AqdS_sFvt1GJiyvg",
       accounts: [
         "403f1a6dd0b2abe03cb942bf50181e9193637d0423356371e75f2407fab9a922",
       ],
